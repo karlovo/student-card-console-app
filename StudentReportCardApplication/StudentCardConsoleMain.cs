c72a38468c5739eeb9c97295754331cd8b28b469
Stoyan.Veleshki <stoyan.veleshki@gmail.com>
2018-03-25 19:39:52 +0300

    init commit

---
+++ StudentReportCardApplication/StudentCardConsoleMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentReportCardApplication
+{
+    class StudentCardConsoleMain
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
